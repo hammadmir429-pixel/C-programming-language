af89be46f1aab08835ea9746aa67f5c03fb2d02b
Hammad Mir <hammadmir429@gmail.com>
2025-11-25 09:51:14 +0500

    Add files via upload

---
+++ hammad Mir lab2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "-----------------------------\n";
+    cout << "\tStudent ID Card\n";
+    cout << "-----------------------------\n\n";
+
+    cout << "Name:\t\tHammad Mir\n";
+    cout << "Roll No:\tSet-25-16\n";
+    cout << "Department:\tComputer Science\n";
+    cout << "Session:\t2025-2029\n";
+
+    return 0;
+}
